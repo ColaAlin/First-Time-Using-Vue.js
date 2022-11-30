0228b796dc6ad28dba63a379bc4987341a56cf27
Alin Cola <colaalincristian@gmail.com>
2022-11-30 12:37:40 +0200

    directives

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 const app = Vue.createApp({
-    data() {
-      return {
-          userName: "Alin Cristian Cola",
-          task: "happy coding"
-      };
+  data() {
+    return {
+      userName: "Alin Cristian Cola",
+      task: "happy coding",
+    };
+  },
+  directives: {
+    highlight: {
+      mounted(el) {
+        el.style.border = "2px solid red";
+        el.style.backgroundColor = "salmon";
+        el.style.borderRadius = "20px";
+        el.style.fontSize = "20px";
+        el.style.fontFamily = "Monospace";
+        el.style.fontWeight = "bold";
+        el.style.color = "white";
+      },
     },
-  }).mount("#app");
+  },
+}).mount("#app");
 
-  console.log(app.userName);
+console.log(app.userName);
